341654d64112787a98161477f3ad691fb93ae167
SemiletovAndrey <116448311+SemiletovAndrey@users.noreply.github.com>
2023-02-25 19:05:34 +0300

    realization area circle

--- app/src/main/java/com/example/calculate/MainActivity.kt
+++ app/src/main/java/com/example/calculate/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.calculate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var textAreaCircle: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var square_circle = 0
-
     }
+
+    fun calculateCircleArea(view: View) {
+        val editText = findViewById<EditText>(R.id.edit_text_number_circle)
+        val text = editText.text.toString()
+        var radius = text.toDouble()
+        val pi = 3.14159
+        var area = pi * radius * radius
+        var areaCircle:String = area.toString()
+        textAreaCircle?.setText(areaCircle)
+        textAreaCircle = findViewById(R.id.textAreaCircle)
+    }
+
+
 }
+
+
