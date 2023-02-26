b1be7d484778bb23cb625b2a82e7f05d3519c16f
SemiletovAndrey <116448311+SemiletovAndrey@users.noreply.github.com>
2023-02-26 11:19:54 +0300

    merge all branches in dev

--- app/src/main/java/com/example/calculate/MainActivity.kt
+++ app/src/main/java/com/example/calculate/MainActivity.kt
@@ -9,7 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     var textAreaRectangle: TextView? = null
-
+    var textAreaCircle: TextView? = null
+    var textAreaSquare: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,7 +28,25 @@ class MainActivity : AppCompatActivity() {
         textAreaRectangle?.setText(areaRectangle)
         textAreaRectangle = findViewById(R.id.textAreaRectangle)
     }
-
+    fun calculateCircleArea(view: View) {
+        val editText = findViewById<EditText>(R.id.edit_text_number_circle)
+        val text = editText.text.toString()
+        var radius = text.toDouble()
+        val pi = 3.14159
+        var area = pi * radius * radius
+        var areaCircle:String = area.toString()
+        textAreaCircle?.setText(areaCircle)
+        textAreaCircle = findViewById(R.id.textAreaCircle)
+    }
+    fun calculateSquareArea(view: View) {
+        val editText = findViewById<EditText>(R.id.edit_text_number_square)
+        val text = editText.text.toString()
+        val sideSquare = text.toDouble()
+        val area =  sideSquare * sideSquare
+        val areaSquare:String = area.toString()
+        textAreaSquare?.setText(areaSquare)
+        textAreaSquare = findViewById(R.id.textAreaSquare)
+    }
 
 }
 
